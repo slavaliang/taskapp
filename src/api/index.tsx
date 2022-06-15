5f1fe318de88c48ad97cc75f29afbe804ba6b879
slavaliang <slavaliang@gmail.com>
2022-06-15 20:02:02 +0600

    feat():actual link

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "http://taskapp.vercel.app";
 const $api = axios.create({
     baseURL: BASE_URL
 })
